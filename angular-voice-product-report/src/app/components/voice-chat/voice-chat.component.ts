c319dcbc74567a64d17d2be83802e895bfa15cb3
nkhieu215 <102019148+nkhieu215@users.noreply.github.com>
2025-04-01 13:21:50 +0700

    Add angular-voice-product-report/src/app/components/voice-chat/voice-chat.component.ts 1743488511829

---
+++ angular-voice-product-report/src/app/components/voice-chat/voice-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voice-chat',
+  imports: [],
+  template: `
+    <p>
+      voice-chat works!
+    </p>
+  `,
+  styles: ``
+})
+export class VoiceChatComponent {
+
+}
